931084e3a33c48a449bddf721e4452a16d2f8cd8
Nicolae Olariu <nicolae.olariu@xivic.com>
2014-04-25 16:08:57 +0300

    chart updated when changing data source

--- app/scripts/controllers/chart.js
+++ app/scripts/controllers/chart.js
@@ -3,5 +3,55 @@
 angular
 	.module('chartVisualizerApp')
 	.controller('ChartCtrl', function ($scope) {
+		$scope.chartDataType = 'tsv';
+		$scope.chartDataPath = 'data/sample.tsv';
+		$scope.chartXProperty = 'date';
+		$scope.chartYProperty = 'close';
+		$scope.chartYAxisTitle = 'Profit';
 
+		$scope.types = [
+			{ dataType:'csv', dataPath:'data/sample.csv', xProperty: 'time', yProperty: 'revenue', yAxisTitle: 'Kilos' },
+		    { dataType:'tsv', dataPath:'data/sample.tsv', xProperty: 'date', yProperty: 'close', yAxisTitle: 'Profit' },
+		    { dataType:'json', dataPath:'data/sample.json', xProperty: 'timestamp', yProperty: 'visits', yAxisTitle: 'Visits' }
+		];
+
+		var loadData = function() {
+			switch ($scope.chartDataType) {
+				case 'tsv':
+						d3.tsv($scope.chartDataPath, function(error, data) {
+							$scope.data = data;
+							$scope.$apply();
+						});
+					break;
+				case 'csv':
+						d3.csv($scope.chartDataPath, function(error, data) {
+							$scope.data = data;
+
+							console.log(data);
+
+							$scope.$apply();
+						});
+					break;
+				case 'json':
+						d3.json($scope.chartDataPath, function(error, data) {
+							$scope.data = data;
+							$scope.$apply();
+						});
+					break;
+			}
+		}
+
+		$scope.$watch('m', function(newValue, oldValue) {
+			if (newValue != oldValue) {
+				$scope.chartDataType = newValue.dataType;
+				$scope.chartDataPath = newValue.dataPath;
+				$scope.chartXProperty = newValue.xProperty;
+				$scope.chartYProperty = newValue.yProperty;
+				$scope.chartYAxisTitle = newValue.yAxisTitle;
+
+				loadData();
+			}
+		});
+
+		loadData();
 	});
--- app/scripts/directives/chartvisualizer.js
+++ app/scripts/directives/chartvisualizer.js
@@ -7,13 +7,14 @@ angular
 			scope: {
 				chartSelector: '@',
 				chartSelectorType: '@',
-				chartXAxisTitle: '@',
-				chartYAxisTitle: '@',
+				chartDataSource: '=',
+				chartXAxisCssClass: '@',
+				chartYAxisCssClass: '@',
+				chartXAxisTitle: '=',
+				chartYAxisTitle: '=',
 				chartType: '@',
-				chartDataType: '@',
-				chartDataPath: '@',
-				chartXProperty: '@',
-				chartYProperty: '@'
+				chartXProperty: '=',
+				chartYProperty: '='
 			},
 			templateUrl: '/views/chartVisualizer.html',
 			restrict: 'AE',
@@ -43,44 +44,13 @@ angular
 				 *
 				 *-----------------------------------------------------------------*/
 
-				var loadData = function() {
-						switch (scope.chartDataType) {
-							case 'tsv':
-									d3.tsv(scope.chartDataPath, function(error, data) {
-										data.forEach(function(d) {
-											d[scope.chartXProperty] = d3.time.format("%d-%b-%y").parse(d[scope.chartXProperty]);
-											d[scope.chartYProperty] = +d[scope.chartYProperty];
-										});
-
-										scope.data = data;
-										scope.$apply();
-									});
-								break;
-							case 'csv':
-									d3.csv(scope.chartDataPath, function(error, data) {
-										data.forEach(function(d) {
-											d[scope.chartXProperty] = d3.time.format("%d-%b-%y").parse(d[scope.chartXProperty]);
-											d[scope.chartYProperty] = +d[scope.chartYProperty];
-										});
-
-										scope.data = data;
-										scope.$apply();
-									});
-								break;
-							case 'json':
-									d3.json(scope.chartDataPath, function(error, data) {
-										data.forEach(function(d) {
-											d[scope.chartXProperty] = d3.time.format("%d-%b-%y").parse(d[scope.chartXProperty]);
-											d[scope.chartYProperty] = +d[scope.chartYProperty];
-										});
-
-										scope.data = data;
-										scope.$apply();
-									});
-								break;
-						}
+				var initialize = function() {
+						formatDataSource();
+						plotChart();
 					},
 					plotChart = function() {
+						$(scope.chartSelectorType + scope.chartSelector).children().remove();
+
 						var margin = { top: 20, right: 20, bottom: 30, left: 50 },
 							chart = d3.select(scope.chartSelectorType + scope.chartSelector),
 						    width = +chart.style('width').replace('px', '') - margin.left - margin.right,
@@ -91,62 +61,63 @@ angular
 						    		.range([height, 0]),
 							xAxis = d3.svg.axis()
 								    .scale(x)
-								    .orient("bottom"),
+								    .orient('bottom'),
 							yAxis = d3.svg.axis()
 								    .scale(y)
-								    .orient("left"),
+								    .orient('left'),
 							line = d3.svg.line()
 								    .x(function(d) { return x(d[scope.chartXProperty]); })
 								    .y(function(d) { return y(d[scope.chartYProperty]); }),
 							svg = d3.select(scope.chartSelectorType + scope.chartSelector)
-									.append("svg")
-								    .attr("width", width + margin.left + margin.right)
-								    .attr("height", height + margin.top + margin.bottom)
-									  .append("g")
-									    .attr("transform", "translate(" + margin.left + "," + margin.top + ")"),
-							data = scope.data;
+									.append('svg')
+								    .attr('width', width + margin.left + margin.right)
+								    .attr('height', height + margin.top + margin.bottom)
+									 	.append('g')
+									    	.attr('transform', 'translate(' + margin.left + ',' + margin.top + ')'),
+							data = scope.chartDataSource;
 
 						x.domain(d3.extent(data, function(d) { return d[scope.chartXProperty]; }));
 						y.domain(d3.extent(data, function(d) { return d[scope.chartYProperty]; }));
 
-						var xAxisElement = svg.append("g")
-											.attr("class", "x axis")
-											.attr("transform", "translate(0," + height + ")")
+						var xAxisElement = svg.append('g')
+											.attr('class', scope.chartXAxisCssClass)
+											.attr('transform', 'translate(0,' + height + ')')
 											.call(xAxis),
-							yAxisElement = svg.append("g")
-											.attr("class", "y axis")
+							yAxisElement = svg.append('g')
+											.attr('class', scope.chartYAxisCssClass)
 											.call(yAxis);
 
+						// Adding oY title.
 						if (scope.chartYAxisTitle) {
 							yAxisElement
-								.append("text")
-								.attr("transform", "rotate(-90)")
-								.attr("y", 6)
-								.attr("dy", ".71em")
-								.style("text-anchor", "end")
+								.append('text')
+								.attr('transform', 'rotate(-90)')
+								.attr('x', -height / 2)
+								.attr('y', 10)
+								.attr('dy', '.71em')
+								.style('text-anchor', 'middle')
 								.text(scope.chartYAxisTitle);
 						}
 
-						svg.append("path")
+						svg.append('path')
 							.datum(data)
-							.attr("class", "line")
-							.attr("d", line);
+							.attr('class', 'line')
+							.attr('d', line);
+					},
+					formatDataSource = function() {
+						scope.chartDataSource.forEach(function(d) {
+							d[scope.chartXProperty] = d3.time.format('%d-%b-%y').parse(d[scope.chartXProperty]);
+							d[scope.chartYProperty] = +d[scope.chartYProperty];
+						});
 					};
 
-				/*------------------------------------------------------------------
-				Watchers
-				------------------------------------------------------------------*/
+				initialize();
 
-				scope.$watch('data', function(newValue, oldValue) {
-					if (newValue != oldValue)
-						plotChart();
+				scope.$watch('chartDataSource', function(newVal, oldVal) {
+					if (newVal != oldVal) {
+						initialize();
+					}
 				});
-
-				/*------------------------------------------------------------------
-				Init
-				------------------------------------------------------------------*/
-
-				loadData();
 			}
 		};
 	});
