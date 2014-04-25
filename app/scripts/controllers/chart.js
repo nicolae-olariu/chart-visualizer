d04b7cae496fa0785f7eda294f7235c6d0519302
Nicolae Olariu <nicolae.olariu@xivic.com>
2014-04-25 17:59:10 +0300

    refactored chart.js file

--- app/scripts/controllers/chart.js
+++ app/scripts/controllers/chart.js
@@ -18,36 +18,41 @@ angular
 		];
 		$scope.type = $scope.types[$scope.useThisTypeIndexAsDefault];
 
-		/*------------------------------------------------------------------
-		Defaults
-		------------------------------------------------------------------*/
-
-		$scope.chartDataType = $scope.types[$scope.useThisTypeIndexAsDefault].dataType;
-		$scope.chartDataPath = $scope.types[$scope.useThisTypeIndexAsDefault].dataPath;
-		$scope.chartXProperty = $scope.types[$scope.useThisTypeIndexAsDefault].xProperty;
-		$scope.chartYProperty = $scope.types[$scope.useThisTypeIndexAsDefault].yProperty;
-		$scope.chartYAxisTitle = $scope.types[$scope.useThisTypeIndexAsDefault].yAxisTitle;
-		$scope.chartXAxisTitle = $scope.types[$scope.useThisTypeIndexAsDefault].xAxisTitle;
+		// Defaults chart margins.
 		$scope.chartMargins = { top: 20, right: 20, bottom: 50, left: 100 };
 
-		var getData = function() {
-			Data.loadData($scope.chartDataType, $scope.chartDataPath).promise.then(function(data) {
-				$scope.data = data;
-			});
-		}
+		var setDefaults = function() {
+				$scope.chartDataType = $scope.types[$scope.useThisTypeIndexAsDefault].dataType;
+				$scope.chartDataPath = $scope.types[$scope.useThisTypeIndexAsDefault].dataPath;
+				$scope.chartXProperty = $scope.types[$scope.useThisTypeIndexAsDefault].xProperty;
+				$scope.chartYProperty = $scope.types[$scope.useThisTypeIndexAsDefault].yProperty;
+				$scope.chartYAxisTitle = $scope.types[$scope.useThisTypeIndexAsDefault].yAxisTitle;
+				$scope.chartXAxisTitle = $scope.types[$scope.useThisTypeIndexAsDefault].xAxisTitle;
+			},
+			getData = function() {
+				Data.loadData($scope.chartDataType, $scope.chartDataPath).promise.then(function(data) {
+					$scope.data = data;
+				});
+			},
+			watchDropDown = function() {
+				$scope.$watch('type', function(newValue, oldValue) {
+					if (newValue != oldValue) {
+						$scope.chartDataType = newValue.dataType;
+						$scope.chartDataPath = newValue.dataPath;
+						$scope.chartXProperty = newValue.xProperty;
+						$scope.chartYProperty = newValue.yProperty;
+						$scope.chartXAxisTitle = newValue.xAxisTitle;
+						$scope.chartYAxisTitle = newValue.yAxisTitle;
 
-		$scope.$watch('type', function(newValue, oldValue) {
-			if (newValue != oldValue) {
-				$scope.chartDataType = newValue.dataType;
-				$scope.chartDataPath = newValue.dataPath;
-				$scope.chartXProperty = newValue.xProperty;
-				$scope.chartYProperty = newValue.yProperty;
-				$scope.chartXAxisTitle = newValue.xAxisTitle;
-				$scope.chartYAxisTitle = newValue.yAxisTitle;
-
+						getData();
+					}
+				});
+			},
+			initialize = function() {
+				setDefaults();
+				watchDropDown();
 				getData();
-			}
-		});
+			};
 
-		getData();
+		initialize();
 	});
